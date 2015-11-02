6fe490fca8d7240fdbf945317716131725aeecd0
Matt Bierner <mattbierner@gmail.com>
2015-11-01 16:56:01 -0800

    Added initial site

---
+++ README.md
@@ -0,0 +1,43 @@
+# 80x40 Client
+
+Simple scripts used to automatically update [80x40](https://github.com/git-dot-art/80x40). 
+
+## Components
+
+### Node command line
+Simple tool for manually merging individual pull requests.
+
+```sh
+$ npm install
+$ node index.js \
+    --token=YOUR_GITHUB_OAUTH_TOKEN \
+    --number=PULL_REQUEST_TO_MERGE
+```
+
+### Node Github Webhook Listener
+Simple node webserver that listens for Github webhook pull request events and updates the repo whenever a pull request is created or updated.
+
+```sh
+$ npm install
+$ node server.js \
+    --port=3000 \
+    --token=YOUR_GITHUB_OAUTH_TOKEN \
+    --secret=YOUR_WEBHOOK_SECRET
+```
+
+### 80x40 Merger - `merger/merger`
+Git merge driver for blocks of text. Written in Python3.
+
+To set this up, in the target 80x40 repo edit the `.git/config` file and add following lines:
+
+```sh
+[merge "80x40"]
+    name = Custom merge driver for 80x40 blocks of text.
+    driver = PATH_TO_80x40_CLIENT_REPO/merger/merger %O %A %B %L
+```
+
+## Setting up 80x40
+Configuration options are stored in in `config.json`.
+
+For most cases, just change `local_repository` to point at where 80x40 is stored on your local computer. `user` and `repo` can also be changed to target a different repository on Github.
+
---
+++ js/main.js
@@ -0,0 +1,91 @@
+"use strict";
+
+var REPO_USER = "art-dot-git";
+
+var REPO = "80x40-test";
+
+var FILE = "README";
+
+var GITHUB = "https://github.com/";
+var GITHUB_RAW = "https://raw.githubusercontent.com/";
+var GITHUB_API = "https://api.github.com/";
+
+
+// State
+var blockHistory = [];
+var blockCache = {};
+var currentIndex = 0;
+
+var splitTextBlock = function(text) {
+    var lines = text.split('\n');
+    // Get rid of trailing newline
+    if (lines.length > 0 && lines[lines.length - 1] === '')
+        return lines.slice(0, -1);
+    return lines;
+};
+
+var updateTextBlock = function(block) {
+    $('#text-block').empty().append(block.map(function(x) {
+        return $('<span></span>').text(x).append('<br/>');
+    }));
+};
+
+var getTextBlock = function(revision, f) {
+    var url = GITHUB_RAW + REPO_USER + '/' + REPO + '/' + revision +'/' + FILE;
+    return $.get(url, f)
+};
+
+var getblockHistory = function(f) {
+    var url = GITHUB_API + 'repos/' + REPO_USER + '/' + REPO + '/commits' + "?path=" + FILE;
+    return $.get(url, f)
+};
+
+var onblockHistoryChange = function(sha, index) {
+    var url = GITHUB + REPO_USER + '/' + REPO + '/commit/' + sha;
+    $('#current-label').text(sha).attr('href', url);
+    $('#forward-button').prop('disabled', index === 0);
+    $('#backward-button').prop('disabled', index === (blockHistory.length - 1));
+    $('#latest-button').prop('disabled', index === 0);
+};
+
+var viewblockHistory = function(index) {
+    var item = blockHistory[index];
+    if (!item)
+        return;
+    
+    var sha = item.sha;
+    var current = blockCache[sha];
+    var label = sha;
+    if (current) {
+        currentIndex = index;
+        updateTextBlock(current);
+        onblockHistoryChange(label, index);
+    } else {
+        getTextBlock(sha, function(text) {
+            currentIndex = index;
+            var block = splitTextBlock(text);
+            blockCache[sha] = block;
+            updateTextBlock(block);
+            onblockHistoryChange(label, index);
+        });
+    }
+};
+
+$(function() {
+    getblockHistory(function(data) {
+        blockHistory = data;
+        viewblockHistory(0);
+    });
+    
+    $('#latest-button').click(function() {
+        viewblockHistory(0);
+    });
+    
+    $('#forward-button').click(function() {
+        viewblockHistory(currentIndex - 1);
+    });
+    
+    $('#backward-button').click(function() {
+        viewblockHistory(currentIndex + 1);
+    });
+});
